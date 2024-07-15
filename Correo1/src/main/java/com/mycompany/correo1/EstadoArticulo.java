7ddb475b2b1d3ec672b3d3079312ea78589c12ce
bot-EdGar <edgnigar@espol.edu.ec>
2024-07-14 22:29:05 -0500

    Delete Correo1 directory

--- Correo1/src/main/java/com/mycompany/correo1/EstadoArticulo.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.correo1;
-
-/**
- *
- * @author edgar
- */
-public enum EstadoArticulo {
-    INGRESADO,RECHAZAR,PUBLICADO
-}
--- Correo1/src/main/java/com/mycompany/correo1/RolUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.correo1;
-
-/**
- *
- * @author edgar
- */
-public enum RolUsuario {
-    AUTOR,REVISOR,EDITOR
-}
